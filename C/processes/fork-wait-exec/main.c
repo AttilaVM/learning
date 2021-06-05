6c31c634a55631f80be6a108c85ec61747685282
Attila V. Molnar <attila.molnar@egroup.hu>
2021-06-05 09:47:13 +0000

    feat: show environment can be set in a fork before exec

--- C/processes/fork-wait-exec/main.c
+++ C/processes/fork-wait-exec/main.c
@@ -4,6 +4,17 @@
 #include <string.h>
 #include <sys/wait.h>
 
+/*
+ * Fork a process
+ * Set the environment in the child process
+ * Run exec to replace the process image of the child
+ *
+ * Parent waits until the child machine state is zombie
+ * Then the parent is terminated and a consequence its child too
+ *
+ * This is basicly the same how UNIX shell start any command
+ */
+
 int main(int argc, char *argv[]) {
     printf("Parent process starts\n");
     int rc = fork();
@@ -11,11 +22,14 @@ int main(int argc, char *argv[]) {
        fprintf(stderr, "fork is failed\n") ;
     }
     else if (rc == 0) {
-        sleep(1);
-        char* wc_args[3];
-        wc_args[0] = strdup("wc");
-        wc_args[1] = strdup("main.c");
-        wc_args[2] = NULL;     // end of array
+        // we successfuly forked now we set a
+        // environment variable before running exec
+        putenv("FOO=bar");
+        char* wc_args[4];
+        wc_args[0] = strdup("bash");
+        wc_args[1] = strdup("-c");
+        wc_args[2] = strdup("echo $FOO");
+        wc_args[3] = NULL;     // end of array
         execvp(wc_args[0], wc_args);
         // exec will overrida the process image of the calling  process
         // which will be replaced by the process image of the new one
